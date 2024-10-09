c10fe4519721a247701ec6b352c22b2790a3be89
Mfinda Adriano <mfindaadriano@gmail.com>
2024-10-09 16:39:47 +0100

    Ready to perform the final test

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const fs = require('fs');
+require('dotenv').config();
 
-var app = express();
+const app = express();
+const port = process.env.PORT; //|| 3000;
 
 app.use(cors());
+//app.use(express.urlencoded({extended: false}));
+//app.use(express.json());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
+  console.log("Home Page");
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+// multer configuration before uploading the multipart/form-data file
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploadFiles/');
+  },
+  filename: (req, file, cb) => {
+    //cb(null, file.fieldname + '-' + Date.now() + ".pdf");
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage });
+
+//app.post('/api/fileanalyse/:name/:type/:size', (req, res) => {
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    const output = {name: req.file.originalname, type: req.file.mimetype, size: req.file.size};
+
+    console.log(output);
+    res.json(output);
+})
 
 
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
